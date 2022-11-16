eaee9d6dc014f6502593284d71ff76ad98d30d1e
Levdbas <Levdbas@users.noreply.github.com>
2022-11-16 20:05:23 +0000

    deploy: cf1b472d393b23b806704e2cafccaa4458266af4

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "f0e45107-6fbda47ff26aa8d46e67.js"
   },
   {
-    "url": "app-64040d5583e8a7b03f54.js"
+    "url": "app-1b49095e03ac5dec18f1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "69358dc66bc781a39254b4ebfd1df10e"
+    "revision": "a58f9f63622dff29b6200786e1dbe319"
   },
   {
     "url": "polyfill-79b4104d7b3ab6e54691.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/wp-lemon-docs/app-64040d5583e8a7b03f54.js`))) {
+  if (!resources || !(await caches.match(`/wp-lemon-docs/app-1b49095e03ac5dec18f1.js`))) {
     return await fetch(event.request)
   }
 
