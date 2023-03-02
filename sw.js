f277d26d77e8e18baeb5a69d4387b04eb4f51ab5
Levdbas <Levdbas@users.noreply.github.com>
2023-03-02 17:18:10 +0000

    deploy: c84e6f283b8424ec8473dda09aa620e9e74aa13c

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "framework-170fc9937e828284989c.js"
   },
   {
-    "url": "45f5fd45-d1590c4de3021631fb30.js"
+    "url": "45f5fd45-4666470ad533504b5b4a.js"
   },
   {
     "url": "f0e45107-6fbda47ff26aa8d46e67.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7918c01a2a2fc6dcd435493629b27d0b"
+    "revision": "ac6ec262b32bde86ba9246f83b14648a"
   },
   {
     "url": "polyfill-79b4104d7b3ab6e54691.js"
