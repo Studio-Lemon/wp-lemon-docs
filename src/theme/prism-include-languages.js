177a536b92ee7b8a290c8c6d3de1f7fec01c47e1
Erik van der Bas <erik@basedonline.nl>
2026-02-14 15:52:44 +0100

    fix: Clean up whitespace in prism-include-languages.js

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -18,7 +18,7 @@ export default function prismIncludeLanguages(PrismObject) {
   require('prismjs/components/prism-markup-templating.js');
   require(`prismjs/components/prism-php.js`);
   require(`prismjs/components/prism-scss.js`);
-  
+
   // Use our custom enhanced Twig syntax definition
   twigLanguage(PrismObject);
 
