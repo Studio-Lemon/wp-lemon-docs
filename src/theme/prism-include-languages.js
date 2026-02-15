808d7f678da520687362cf7fee1e7579a97f9451
Erik van der Bas <erik@basedonline.nl>
2026-02-15 18:07:20 +0100

    feat: Add support for Bash syntax highlighting in Prism

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -17,6 +17,7 @@ export default function prismIncludeLanguages(PrismObject) {
 
   require('prismjs/components/prism-markup-templating.js');
   require(`prismjs/components/prism-php.js`);
+  require(`prismjs/components/prism-bash.js`);
   require(`prismjs/components/prism-scss.js`);
 
   // Use our custom enhanced Twig syntax definition
